74c14283832e91e8e306d7aa9afcdb758007ba5d
Kevin Kelly <ircmullaney@gmail.com>
2023-04-01 16:06:07 -0500

    Setting up basic layout with an example of a custom layout

---
+++ layouts/custom.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    The custom layout!
+    <slot />
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This uses a custom layout</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'custom'
+  })
+</script>
