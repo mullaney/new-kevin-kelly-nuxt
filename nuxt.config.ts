afe2ac1c195fe73880fda1e23a38f0b60ffb36dc
Kevin Kelly <ircmullaney@gmail.com>
2023-04-03 08:07:26 -0500

    WIP with import error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: [
-    '@/assets/style/global.css',
+    // '@/assets/style/global.css',
     '~/assets/style/content.scss'
   ],
+  extends: '@nuxt-themes/typography',
   modules: [
     '@nuxt/content'
   ],
