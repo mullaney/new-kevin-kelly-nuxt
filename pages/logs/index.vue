7db945a5df8c71b5d1d37adf5382bb7f8ee8ef9e
Kevin Kelly <ircmullaney@gmail.com>
2023-04-08 08:04:46 -0500

    Basic log page

---
+++ pages/logs/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section>
+      <article
+        v-for="log in logPosts"
+        :key="log._id"
+      >
+        <h2>
+          <NuxtLink :to="log._path">
+            {{ log.title }}
+          </NuxtLink>
+        </h2>
+      </article>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+const logPosts = await queryContent('/logs').find();
+
+</script>
+
+<style scoped>
+h2 a {
+  color: black;
+  text-decoration: none;
+}
+
+</style>
